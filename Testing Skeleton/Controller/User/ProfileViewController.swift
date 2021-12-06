af8d90f3f8b1f8d4dd1c2f9ee112d2beb04c55f2
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2021-12-07 06:42:37 +0700

    Make if else condition if user already login or not in Profile Screen Added logic code to delete NSUserDefault if user want to logout Added login bar button in Profile Screen Added Illustration image programmaticaly in Profile Screen

--- Testing Skeleton/Controller/User/ProfileViewController.swift
+++ Testing Skeleton/Controller/User/ProfileViewController.swift
@@ -13,20 +13,56 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userEmail: UILabel!
     @IBOutlet weak var userBirth: UILabel!
+    @IBOutlet weak var loginBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var stackViewOne: UIStackView!
+    @IBOutlet weak var stackViewTwo: UIStackView!
+    @IBOutlet weak var logOutButton: UIButton!
     
     
     // Variables
     let userDefault = UserDefaults.standard
     
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        userName.text = userDefault.object(forKey: "userName") as? String
-        userEmail.text = userDefault.object(forKey: "userEmail") as? String
-        userBirth.text = userDefault.object(forKey: "userBirthDate") as? String
+        if userDefault.object(forKey: "userLoginKey") as? String != nil {
+            print("token available")
+            userName.text = userDefault.object(forKey: "userName") as? String
+            userEmail.text = userDefault.object(forKey: "userEmail") as? String
+            userBirth.text = userDefault.object(forKey: "userBirthDate") as? String
+            loginBarButtonItem.isEnabled = false
+            self.navigationItem.setRightBarButton(nil, animated: true)
+        } else {
+            print("token not available")
+            getImage()
+            stackViewOne.isHidden = true
+            stackViewTwo.isHidden = true
+            logOutButton.isHidden = true
+        }
         
+    }
+    
+    func getImage() {
+        let illustrationImage = UIImage(named: "loginImage.png")
+        
+        let myImageView:UIImageView = UIImageView()
+        myImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        myImageView.frame.size.width = 300
+        myImageView.frame.size.height = 300
+        myImageView.center = self.view.center
+        myImageView.image = illustrationImage
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        label.font = UIFont.boldSystemFont(ofSize: 16.0)
+        label.center = self.view.center
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Harap login dahulu untuk menikmati fitur ini"
+
+        
+        self.view.addSubview(myImageView)
+        self.view.addSubview(label)
     }
     
 
@@ -47,6 +83,12 @@ class ProfileViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: UIButton) {
         APIManager.shareInstance.callingLogoutAPI(self)
         userDefault.removeObject(forKey: "userName")
+        userDefault.removeObject(forKey: "userEmail")
+        userDefault.removeObject(forKey: "userAddress")
+        userDefault.removeObject(forKey: "userGender")
+        userDefault.removeObject(forKey: "userBirthDate")
+        userDefault.removeObject(forKey: TokenKey.userLogin)
+        
         print("Keluar Pressed")
         
         let storyBoard: UIStoryboard = UIStoryboard(name: "Login", bundle: nil)
@@ -55,6 +97,15 @@ class ProfileViewController: UIViewController {
         viewController.navigationItem.hidesBackButton = true
         viewController.hidesBottomBarWhenPushed = true
         self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    
+    @IBAction func loginBarButtonPressed(_ sender: UIBarButtonItem) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Login", bundle: nil)
+        let loginView = storyBoard.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        loginView.fromHome = true
+        loginView.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(loginView, animated: true)
     }
     
 }
