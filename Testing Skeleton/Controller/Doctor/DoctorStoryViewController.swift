86b62e893b9eefe392ddd87beec19e914178ee63
Geadalfa Giyanda <ggeadalfa@gmail.com>
2021-12-06 12:09:13 +0700

    Adding Detail Doctor (DoctorStory) storyboard

---
+++ Testing Skeleton/Controller/Doctor/DoctorStoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DoctorStoryViewController.swift
+//  Testing Skeleton
+//
+//  Created by Geadalfa Giyanda on 06/12/21.
+//
+
+import UIKit
+
+class DoctorStoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
