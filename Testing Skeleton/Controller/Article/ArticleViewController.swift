1cf80b86843777769fccbefe801ed166c94ea4e1
JovaN <jovanalvin@gmail.com>
2021-12-07 21:10:09 +0700

    Article: Fetch Article From API Done. (-)Image SDImage

--- Testing Skeleton/Controller/Article/ArticleViewController.swift
+++ Testing Skeleton/Controller/Article/ArticleViewController.swift
@@ -16,7 +16,7 @@ class ArticleViewController: UIViewController {
     
     // Variables
     let articleModel = ArticleModel()
-    
+    var ModelArticle = FetchArticle(status: "", totalResults: 0, articles: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,7 @@ class ArticleViewController: UIViewController {
         
         articleTableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: "articleIdentifier")
         articleTableView.rowHeight = 100
+        displayData()
     }
     
     
@@ -33,28 +34,31 @@ class ArticleViewController: UIViewController {
 extension ArticleViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return articleModel.article.count
+        return ModelArticle.totalResults
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cellArticle = collectionView.dequeueReusableCell(withReuseIdentifier: "articleCollectionIdentifier", for: indexPath) as! ArticleCellCollection
-        let index = articleModel.article[indexPath.row]
-        let image = UIImage(named: "\(index.image)")
-        cellArticle.articleImageView.image = image
-        cellArticle.articleLabel.text = index.title
+        let index = ModelArticle.articles?[indexPath.row]
+        //let index = articleModel.article[indexPath.row]
+        //let image = UIImage(named: "\(index.image)")
+        //cellArticle.articleImageView.image = image
+        cellArticle.articleLabel.text = index?.title ?? ""
         
         return cellArticle
         
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let indexPath = articleModel.article[indexPath.row]
+        let indexPath = ModelArticle.articles?[indexPath.row]
         
         let storyBoard: UIStoryboard = UIStoryboard(name: "Article", bundle: nil)
         let detailArticleVC = storyBoard.instantiateViewController(withIdentifier: "DetailArticleController") as! DetailArticleViewController
-        detailArticleVC.articleTitle = indexPath.title
-        detailArticleVC.articleImage = indexPath.image
-        detailArticleVC.articleContent = indexPath.content
+        detailArticleVC.articleTitle = indexPath?.title
+        //detailArticleVC.articleImage = indexPath.image
+        detailArticleVC.articleAuthor = indexPath?.author
+        detailArticleVC.articleDate = indexPath?.publishedAt
+        detailArticleVC.articleContent = indexPath?.content
         detailArticleVC.hidesBottomBarWhenPushed = true // Removing bottom bar in detail article screen
         self.navigationController?.pushViewController(detailArticleVC, animated: true)
     }
@@ -66,29 +70,32 @@ extension ArticleViewController: UICollectionViewDelegate, UICollectionViewDataS
 // MARK: - UITableView
 extension ArticleViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articleModel.article.count
+        return ModelArticle.totalResults
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "articleIdentifier", for: indexPath) as! ArticleTableViewCell
-        let index = articleModel.article[indexPath.row]
-        cell.articleImageView.image = UIImage(named: "\(index.image)")
-        cell.articleTitleLabel.text = index.title
-        cell.articleContentLabel.text = index.content
+        let index = ModelArticle.articles?[indexPath.row]
+        //let index = articleModel.article[indexPath.row]
+        //cell.articleImageView.image = UIImage(named: "\(index.image)")
+        cell.articleTitleLabel.text = index?.title ?? ""
+        cell.articleContentLabel.text = index?.description ?? ""
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let index = articleModel.article[indexPath.row]
+        let index = ModelArticle.articles?[indexPath.row]
         
         let storyBoard: UIStoryboard = UIStoryboard(name: "Article", bundle: nil)
         let detailArticleVC = storyBoard.instantiateViewController(withIdentifier: "DetailArticleController") as! DetailArticleViewController
-        detailArticleVC.articleTitle = index.title
-        detailArticleVC.articleImage = index.image
-        detailArticleVC.articleContent = index.content
+        detailArticleVC.articleTitle = index?.title
+        //detailArticleVC.articleImage = index.image
+        detailArticleVC.articleContent = index?.content
+        detailArticleVC.articleAuthor = index?.author
+        detailArticleVC.articleDate = index?.publishedAt
         detailArticleVC.hidesBottomBarWhenPushed = true // Removing bottom bar in detail article screen
         self.navigationController?.pushViewController(detailArticleVC, animated: true)
         self.articleTableView.deselectRow(at: indexPath, animated: true)
@@ -97,4 +104,31 @@ extension ArticleViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     
+}
+
+//MARK: Fetch Article Data
+extension ArticleViewController {
+    func displayData() {
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=id&apiKey=c910bfd484464746b4c911b0615c1028") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let decodedPosts = try? JSONDecoder().decode(FetchArticle.self, from: data) {
+                    self.ModelArticle = decodedPosts
+                    DispatchQueue.main.async {
+                        //self.activityIndicatorView.stopAnimating()
+                        //self.activityIndicatorView.isHidden = true
+                        print(self.ModelArticle)
+                        print("Reload")
+                        self.articleCollectionView.reloadData()
+                        self.articleTableView.reloadData()
+                    }
+                } else {
+                    debugPrint("Failure to decode posts.")
+                    print(error?.localizedDescription)
+                }
+            } else {
+                debugPrint("Failure to get data.")
+            }
+        }.resume()
+    }
 }
