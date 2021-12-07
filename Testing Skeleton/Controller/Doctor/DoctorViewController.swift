1c88c366666da1e4e33664f9f364e6f91a661659
Geadalfa Giyanda <ggeadalfa@gmail.com>
2021-12-07 17:46:21 +0700

    Change doctor API url in Doctor View Controller

--- Testing Skeleton/Controller/Doctor/DoctorViewController.swift
+++ Testing Skeleton/Controller/Doctor/DoctorViewController.swift
@@ -34,7 +34,7 @@ class DoctorViewController: UIViewController {
     
     func displayData() {
         
-        guard let url = URL(string: "https://61a9916133e9df0017ea3e3d.mockapi.io/users") else { return }
+        guard let url = URL(string: "https://61a9916133e9df0017ea3e3d.mockapi.io/doctor") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let data = data {
